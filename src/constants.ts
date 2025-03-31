2b2da9b4264fb6d31278275a77fd1818928b8e2f
zhaolu <axelulue@gmail.com>
2025-03-31 11:06:30 +0800

    chore: update site branding and add .vercel to gitignore
    - Change site domain from xqy404.com to axelu.me
    - Update author name from Quentin Hsu to Axel Lu
    - Replace GitHub avatar URL in header
    - Add .vercel directory to gitignore

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
-export const SITE = 'xqy404.com'
-export const SITE_TITLE = 'Quentin Hsu'
-export const AUTHOR = 'Quentin Hsu'
+export const SITE = 'axelu.me'
+export const SITE_TITLE = 'Axelu'
+export const AUTHOR = 'Axel Lu'
 export const SITE_DESCRIPTION = 'Welcome to my website!'
